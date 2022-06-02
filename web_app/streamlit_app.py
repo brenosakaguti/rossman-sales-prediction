99355a0c980c657837bf3bee7bf252311c6e4a08
Breno Sakaguti <brenosakaguti@gmail.com>
2022-06-02 17:18:21 -0300

    Started streamlit app

---
+++ web_app/streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+
+data = pd.read_csv("computed_data.csv")
+data["Date"] = data["Date"].astype("datetime64")
+
+st.title("Rossmann Sales Prediction")
+store_number = st.selectbox("Select store number:", data["Store"].unique())
+
+filtered_data = data[data["Store"] == store_number]
+
+sum = int(filtered_data["Sales"].sum())
+st.subheader("The store " + str(store_number) + " will sell " + str(sum) + " units in the time period")
+
+c = alt.Chart(filtered_data).mark_line().encode(x="Date", y="Sales")
+st.altair_chart(c, use_container_width=True)
